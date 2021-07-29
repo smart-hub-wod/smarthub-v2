4a92f5915347bd40a967a37e33bdd7bc3d888ffa
komalvachhani <komal.vachhani@outlook.com>
2021-07-28 23:09:01 -0400

    initial privacy + terms pages commit

--- src/privacy.js
+++ src/privacy.js
@@ -3,7 +3,13 @@ import React from "react"
 export default function Privacy() {
     return (
         <div>
-            <h1>[Privacy Policy]</h1>
+            <h1 className="text-shblue pt-3 text-center">Privacy Policy</h1>
+            <p>Insert privacy policy content here!!!</p>          
+
+            <input id="myinput" type="checkbox" />
+            <label for="myinput" class="indented-checkbox-text">I agree!</label>           
+
         </div>
+        
     )
-}
+}   
