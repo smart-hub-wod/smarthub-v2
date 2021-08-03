944179510eb831d7fcf29361277eb1d65875896a
Jun <junseokim0865@gmail.com>
2021-08-03 12:34:50 -0400

    course listing front-end progressing

--- src/listing.js
+++ src/listing.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react"
 // import firebase from './firebase'
 import firebase from 'firebase/app'
 import { useParams } from "react-router";
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form, Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useAuth } from "./contexts/AuthContext.js"
+import { ChatRight } from "react-bootstrap-icons";
 
 export default function Listing() {
     let { id } = useParams();
@@ -70,24 +71,68 @@ export default function Listing() {
 
     return (
         <>
-            <div>
-                <h1 className="text-center text-shblue mt-5">{course.title}</h1>
-                <h4 className="text-center text-secondary mb-4">{course.timeline}</h4>
+            
+            {/* How can I use the course.bannerImg variable i added onto firebase? */}
+            <div className="course-banner-img w-100" stlye={{backgrondImage: `url(${course.bannerImg})`}}/>
+            {console.log(course.bannerImg)}
+
+            <div className="course-banner">
+                
+                <Container>
+                    <h1 className="text-center text-white mt-5">{course.title}</h1>
+                    <h4 className="text-center text-white text-secondary mb-4">{course.timeline}</h4>
+                </Container>
+                
             </div>
-            <div className="row align-items-start justify-content-center my-4">
-                <div className="col-6 text-white is-shblue p-4 rounded">
-                {course.description}
-                </div>
-                <div className="col-4 text-center">
-                    <select className="form-select mb-3" defaultValue="0" aria-label="Default select example" id="nameSelect">
-                        {kids ? kids.map((kid) => {
-                            return (<option value={kid}>{kid}</option>)
-                        }) : <h1>Loading</h1>}
-                    </select>
-                    <Button bsPrefix="button-sh" onClick={addCourse}>Add to Cart</Button>
-                    <p className="text-shblue mt-1">Only <span className="fs-3 fw-bold">${course.price} </span></p>
-                </div>
-            </div>
+            {/* <div className="text-center w-100 course-container"> */}
+                <Container className="course-container px-3 pt-4">
+                    <div className="row align-items-start justify-content-center ">
+                        <div className="col-4 text-center">
+                            <img className="img-fluid m-3" src="https://th.bing.com/th/id/OIP.TCSkGnIfl1fIQieMpI1RVAHaE7?w=246&h=180&c=7&o=5&pid=1.7" alt="course-pic" />
+                            <h2 className="text-shblue text-align-left my-4"> Choose your student: </h2>
+                            <select className="form-select mb-3" defaultValue="0" aria-label="Default select example" id="nameSelect">
+                                    {kids ? kids.map((kid) => {
+                                        return (<option value={kid}>{kid}</option>)
+                                    }) : <h1>Loading</h1>}
+                            </select>
+                            
+                            <p className="text-shblue mt-1">Only <span className="fs-3 fw-bold">${course.price} </span></p>
+                        </div>
+                        <div className="col-6 text-white is-shblue py-4 mt-3 rounded">
+
+                            <h2> Software Engineering </h2>
+                            {course.description}
+
+                            <hr/>
+                            <Button className="w-100" bsPrefix="button-sh" onClick={addCourse}> Add to Cart</Button>
+                            <hr/>
+                            <Row>
+                                <Col className="justify-content-center" xs={6}>
+                                    <Button bsPrefix="button-sh" size="lg"> Add to wish list</Button>
+                                </Col>
+
+                                <Col xs={6}>
+                                    <h3> bunch of icons </h3>
+                                </Col>
+                            </Row>
+                            <div className="course-includes">
+                                <h2> Includes: </h2>
+                            {/* use some sort of looping? */}
+                                <Row>
+                                    <Col>
+                                        <h3> Articles </h3>
+                                    </Col>
+                                    <Col>
+                                        6
+                                    </Col>
+                                </Row>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </Container>
+            {/* </div> */}
         </>
     )
 }
