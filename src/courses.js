f2f147599fff4a8a9ef4cefb577ef44b289057e6
khloe-r <khloe.ramdhan@outlook.com>
2021-08-08 22:21:17 -0400

    fixed search bar + implemented cloud storage images

--- src/courses.js
+++ src/courses.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import firebase from './firebase'
 import 'firebase/storage';  
 import { Card, Button } from 'react-bootstrap'
@@ -10,6 +10,9 @@ export default function Courses() {
     const [loading, setLoading] = useState(false);
     const [filtered, setFiltered] = useState(false);
     const [newCourses, setNewCourses] = useState([]);
+    var temp = []
+    const [searchValue, setSearchValue] = useState('')
+    const searchRef = useRef()
     var storageRef = firebase.storage()
     // var coverRef = storageRef.ref(`${id}/${id}.jpeg`);
                     // coverRef.getDownloadURL()
@@ -22,51 +25,71 @@ export default function Courses() {
 
     function getCourses() {
         setLoading(true);
+        const items = [];
         courseref.onSnapshot((querySnapshot) => {
-            const items = [];
             querySnapshot.forEach((doc) => {
-                
                 const course = doc.data()
                 var coverRef = storageRef.ref(`${course.id}/${course.id}.jpeg`);
                 coverRef.getDownloadURL()
                 .then((URL) => {
                     course.url = URL
                 }).then(() => {
-                    console.log(course.url)
-                })
-                items.push(course) 
-                             
-            })
+                    //console.log(course.url)
+                })          
+                items.push(course)  
+            })   
             setCourses(items)
-            setLoading(false)       
+            console.log(courses)
+              
+            //searchRef.value = 'a'
+            
+            // setLoading(false)
+            // setTimeout(setLoading(true), 100)
+            setTimeout(() => {
+                // setSearchValue('')
+                searchCourses()
+                // resetSearch()
+                setLoading(false)
+            }, 200);
+            // setSearchValue('a')
+            // searchCourses()
+            // resetSearch()
+            // setLoading(false)
+            
         })
     }
 
     useEffect(() => {
         getCourses();
+        console.log("hello")
+        
     }, [])
 
     function searchCourses() {
-        let search = document.getElementById("search-bar")
-        let previewDiv = document.getElementById("course-previews")
-        if (search.value === "") {
-            setFiltered(false)
-        } else {
-            setNewCourses(courses.filter((course) => course.title.toLowerCase().includes(search.value.toLowerCase())))
-            console.log(search.value + " " + newCourses.length)
-            if (newCourses.length === 0) {
-                
-            } else {
+        temp = []
+
+            courses.map((c) => {
+                let s = (c.title.toLowerCase().includes(searchValue.toLowerCase()))
+                if (s) {
+                    temp.push(c)
+                }
+            })
+            setNewCourses([...temp])
+            if (temp.length > 0) {
                 setFiltered(true)
+            } else {
+                setFiltered(false)
             }
-        }
         
     }
 
+    const handleSearchInput = (e) => {
+        setSearchValue(e.target.value)
+    }
+
     function resetSearch() {
-        let search = document.getElementById("search-bar")
-        search.value = ""
-        searchCourses()
+        setFiltered(false)
+        setSearchValue('')
     }
 
     if(loading) {
@@ -77,18 +100,20 @@ export default function Courses() {
         <div>
             <h1 className="text-shblue pt-3 text-center">Courses</h1>
             <div className="text-start mx-5 d-flex justify-content-center">
-                <input type="text" className="form-control" placeholder="Search for Courses..." id="search-bar" aria-label="Search for Courses" aria-describedby="Search bar for course selection" />
+                <input type="text" value={searchValue} onChange={handleSearchInput} className="form-control" placeholder="Search for Courses..." id="search-bar" aria-label="Search for Courses" aria-describedby="Search bar for course selection" />
                 <Button bsPrefix="button-sh" onClick={searchCourses}><Search color="white"/></Button>
             </div>
             <div className="text-start mx-5 pt-3 d-flex justify-content-center">
                 <Button bsPrefix="button-sh" className="text-center mb-3" onClick={resetSearch}>Reset Search</Button>
             </div>
             <div id="course-previews">
-                {filtered ?
-                newCourses.map((course) => (
+                {console.log("TEMPPPP", temp)}
+                {(filtered ? newCourses : courses).map((course) => (
+                
                     <Card className="p-3 mx-5 mb-3">
                         <div className="row">
                         <div className="col-3 overflow-hidden">
+                            {console.log(course.url)}
                             <img height="250" src={course.url} />
                         </div>
                         <div key={course.id} className="col-9">
@@ -100,21 +125,22 @@ export default function Courses() {
                     </Card>
                     
                 ))
-                : courses.map((course) => (
-                    <Card className="p-3 mx-5 mb-3">
-                        <div className="row">
-                        <div className="col-3 overflow-hidden">
-                            <img height="250" src={course.url} />
-                        </div>
-                        <div key={course.id} className="col-9">
-                            <h2>{course.title}</h2>
-                            <p>{course.description}</p>
-                            <Link to={`/course-listing/${course.id}`}><Button bsPrefix="button-sh">View Course</Button></Link>
-                        </div>
-                        </div>
-                    </Card>
+                // : courses.map((course) => (
+                //     <Card className="p-3 mx-5 mb-3">
+                //         <div className="row">
+                //         <div className="col-3 overflow-hidden">
+                //             <img height="250" src={course.url} />
+                //         </div>
+                //         <div key={course.id} className="col-9">
+                //             <h2>{course.title}</h2>
+                //             <p>{course.description}</p>
+                //             <Link to={`/course-listing/${course.id}`}><Button bsPrefix="button-sh">View Course</Button></Link>
+                //         </div>
+                //         </div>
+                //     </Card>
                     
-                ))}
+                // ))}
+                }
             </div>
         </div>
     )
