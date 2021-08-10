911863ae49c2fcb540ac25f795846d4d21d14667
Jun <junseokim0865@gmail.com>
2021-08-10 17:30:54 -0400

    contact page update

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,6 @@
 import React from "react";
-import {
-  Container,
-  Form,
-  Row,
-  Col,
-  Button,
-} from "react-bootstrap";
-// import { Front } from "react-bootstrap-icons";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import { PhoneVibrate } from "react-bootstrap-icons";
 
 // breakdown this page
 // set up the layout
@@ -22,24 +16,69 @@ export default function Contact() {
       }}
     >
       <div>
-        <h1 className="heading-text">
-          Contact Us
-        </h1>
+        <h1 className="heading-text">Contact Us</h1>
         <hr />
       </div>
+      {/* Message part start  */}
+      <h2 className="text-shblue" style={{ fontWeight: "600" }}>
+        {" "}
+        Send us your Message{" "}
+      </h2>
+      
+      <p>
+        {" "}
+        When your send your email, please indicate the brief topic of your query
+        on the email title.
+        <br /> We take emails from 8 am to 3 pm.{" "}
+        <br/> Mail us to <a href="mailTo:smarthubteam@onamap.ca"> here </a>
+      </p>
 
-      <Form>
+      {/* Call part start */}
+
+      <h2 className="text-shblue" style={{ fontWeight: "600" }}>
+        {" "}
+        Send us your Call{" "}
+      </h2>
+      <p>Our phone service is open from 8am - 3pm EST </p>
+      <Row>
+        <Col xs={1}>
+          <PhoneVibrate size={100}> </PhoneVibrate>
+        </Col>
+        <Col xs={10} className="ps-5">
+          <p style={{ display: "inline", fontSize: "1.5rem" }}>
+            {" "}
+            Canada (+1) <br /> 920-025-5020{" "}
+          </p>
+        </Col>
+      </Row>
+      <p> We are waiting for your call</p>
+
+      {/* Intructor contact start */}
+
+      <h2 className= "text-shblue"> Contact your instructor </h2>
+
+      <p> Your instructor will be available when you put your request. </p>
+
+      <h2 className="text-shblue"> More information about Company </h2>
+      
+      <a href=""> About West Oak Design Inc. </a>
+
+      
+
+      <p></p>
+
+      {/* <Form action="mailTo:smarthubteam@onamap.ca">
         <Row>
           <Col xs={6}>
             <Form.Group>
               <Form.Label className="my-4 label-text">
-                First Name
+                Name
               </Form.Label>
 
               <Form.Control
                 className="input"
                 type="text"
-                placeHolder="type your first name"
+                placeHolder="type your name"
                 required
               />
             </Form.Group>
@@ -116,7 +155,7 @@ export default function Contact() {
             className="px-5"
           />
         </Form.Group>
-      </Form>
+      </Form>*/}
     </Container>
   );
 }
